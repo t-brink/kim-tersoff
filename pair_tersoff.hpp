c2bdb29e259dd8de43e7f164574f121028123f42
Tobias Brink <brink@mm.tu-darmstadt.de>
2013-11-06 16:49:50 +0100

    Undo memory layout change for publishing parameters.
    This incurs a slowdown, which in limited benchmarking can be 5%.
    Instead store parameters twice and copy on reinit().  This increases
    memory usage but does not slow the calculation down.

--- pair_tersoff.hpp
+++ pair_tersoff.hpp
@@ -98,20 +98,78 @@ class PairTersoff /*: public Pair*/ {
 
 
  private:
+  // Parameters are stored together in a struct because I found that
+  // using the layout that conforms to KIM (one array per parameter)
+  // can lead to a slowdown of up to 5% because the data is not in
+  // adjacent memory.  The parameter data is not so much, so we will
+  // copy between the KIM-published parameters and this internal data.
   struct Params {
-    explicit Params(int N) // Number of particle types
+    double A, B;
+    double lam1, lam2, lam3;
+    double c, d, h;
+    double gamma;
+    int m;
+    double n, beta;
+    // Cutoff related.
+    double D, R;
+    double cut, cutmin, cutsq;
+    // Pre-computed.
+    double n_precomp[4];
+    double c2;    // c^2
+    double d2;    // d^2
+    double c2_d2; // c^2 / d^2
+  };
+  struct KIMParams {
+    explicit KIMParams(int N) // Number of particle types
       : A(N,N,N), B(N,N,N),
         lam1(N,N,N), lam2(N,N,N), lam3(N,N,N),
         c(N,N,N), d(N,N,N), h(N,N,N),
         gamma(N,N,N),
         m(N,N,N),
         n(N,N,N), beta(N,N,N),
-        D(N,N,N), R(N,N,N),
-        cut(N,N,N), cutmin(N,N,N), cutsq(N,N,N),
-        n_precomp(N,N,N,4),
-        c2(N,N,N),
-        d2(N,N,N),
-        c2_d2(N,N,N) {}
+        D(N,N,N), R(N,N,N) {}
+    // Copy data from a Params array.
+    void from_params(const Array3D<Params>& p) {
+      for (int i = 0; i < A.extent(0); ++i)
+        for (int j = 0; j < A.extent(1); ++j)
+          for (int k = 0; k < A.extent(2); ++k) {
+            A(i,j,k) = p(i,j,k).A;
+            B(i,j,k) = p(i,j,k).B;
+            lam1(i,j,k) = p(i,j,k).lam1;
+            lam2(i,j,k) = p(i,j,k).lam2;
+            lam3(i,j,k) = p(i,j,k).lam3;
+            c(i,j,k) = p(i,j,k).c;
+            d(i,j,k) = p(i,j,k).d;
+            h(i,j,k) = p(i,j,k).h;
+            gamma(i,j,k) = p(i,j,k).gamma;
+            m(i,j,k) = p(i,j,k).m;
+            n(i,j,k) = p(i,j,k).n;
+            beta(i,j,k) = p(i,j,k).beta;
+            D(i,j,k) = p(i,j,k).D;
+            R(i,j,k) = p(i,j,k).R;
+          }
+    };
+    // Copy data to a Params array.
+    void to_params(Array3D<Params>& p) const {
+      for (int i = 0; i < A.extent(0); ++i)
+        for (int j = 0; j < A.extent(1); ++j)
+          for (int k = 0; k < A.extent(2); ++k) {
+            p(i,j,k).A = A(i,j,k);
+            p(i,j,k).B = B(i,j,k);
+            p(i,j,k).lam1 = lam1(i,j,k);
+            p(i,j,k).lam2 = lam2(i,j,k);
+            p(i,j,k).lam3 = lam3(i,j,k);
+            p(i,j,k).c = c(i,j,k);
+            p(i,j,k).d = d(i,j,k);
+            p(i,j,k).h = h(i,j,k);
+            p(i,j,k).gamma = gamma(i,j,k);
+            p(i,j,k).m = m(i,j,k);
+            p(i,j,k).n = n(i,j,k);
+            p(i,j,k).beta = beta(i,j,k);
+            p(i,j,k).D = D(i,j,k);
+            p(i,j,k).R = R(i,j,k);
+          }
+    }
     Array3D<double> A, B;
     Array3D<double> lam1, lam2, lam3;
     Array3D<double> c, d, h;
@@ -120,16 +178,12 @@ class PairTersoff /*: public Pair*/ {
     Array3D<double> n, beta;
     // Cutoff related.
     Array3D<double> D, R;
-    // Pre-computed.
-    Array3D<double> cut, cutmin, cutsq;
-    Array4D<double> n_precomp;
-    Array3D<double> c2;    // c^2
-    Array3D<double> d2;    // d^2
-    Array3D<double> c2_d2; // c^2 / d^2
   };
 
   int n_spec;                   // number of species
-  Params params;                // n_spec*n_spec*n_spec arrays of parameters
+  Array3D<Params> params;       // n_spec*n_spec*n_spec array of parameters
+  KIMParams kim_params;         // Parameters published to KIM, see above
+                                // why we keep two copies.
   double max_cutoff;            // max cutoff for all elements
   std::map<int,std::string> to_spec;  // map element index to element
                                       // name, needed for user-
